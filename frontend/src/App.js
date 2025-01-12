5a43ba7d5bbe266aa2bb39caabd28575fedd44f3
Arjun Saji <157756084+Arjun-saji@users.noreply.github.com>
2025-01-12 16:27:26 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+// import TaskList from "./components/Tasklist";
+// import TaskForm from "./components/Taskform";
+// import TaskDetails from "./components/Taskdetails";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//         <h2 className="headingh2">Add Task</h2>
+//         {/* TaskForm is rendered here */}
+//         <TaskForm />
+//         {/* TaskList is rendered below */}
+//         <TaskList />
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
